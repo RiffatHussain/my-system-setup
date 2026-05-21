25d2f8dfa7d0070304858203bc89c038c51427a7
riffat <riffathussainkr@gmail.com>
2026-05-21 14:32:45 +0530

    Updated the last commands of docker cleanup

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -10,8 +10,15 @@ echo "To clean the unused and dangling images i.e that it was not tagged or even
 docker image prune -f
 
 echo "To clean up the volumes that was not used"
-docker volume ls
+docker volume ls --filter dangling=true
 echo "The above volumes are the total volumes"
 
-echo "This command will give you the volumes that are not used and then it will provide along with the 
+echo "This command will give you the volumes that are not used and then it will provide along with the
+size of the volume"
+docker volume ls --filter dangling=true | xargs -I {} /var/lib/docker/volumes/{}/_data 2>/dev/null
+
+echo "To truncate the docker logs"
+find /var/lib/docker/containers -type f -name "*.json.log" -exec truncate s 0 {} \;
+
+
 
